058a824ac3d6f93611398abc2e3993b554b161aa
Frederik Goethals <frederik@frontaal-it.be>
2020-10-26 16:51:07 +0100

    raad getal app v0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'raad-getal-app';
+  title = 'taak1';
+  aantalBeurtenResterend: number = 10;
+  randomNumber: number = Math.floor(Math.random()*100);
+  feedback: string = "";
+  isDisabled = false;
+  
+
+  berekenGok(value: number) {
+    if( this.aantalBeurtenResterend == 0){
+      this.isDisabled = true;
+    } else{
+      if( value == this.randomNumber ){
+        this.feedback = `${value} is gelijk aan het random-getal! HOERA Confetti!`;
+        this.isDisabled = true;
+      } else if( value > this.randomNumber ){
+        this.feedback = `${value} is groter dan random-getal...`;
+      } else if( value < this.randomNumber){
+        this.feedback = `${value} is kleiner dan random-getal...`;
+      }
+      
+      this.aantalBeurtenResterend -= 1;
+    }
+  }
 }
